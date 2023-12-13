fc6363905a0984d9d13bc2289170fbc8028b342c
Reham <reham.shedid@vodafone.com>
2023-12-13 10:03:23 +0200

    Update LineItemSpecification.java

--- dto/src/main/java/ie/vodafone/dxl/checkservicefeasibility/dto/parts/queryancillaryservices/LineItemSpecification.java
+++ dto/src/main/java/ie/vodafone/dxl/checkservicefeasibility/dto/parts/queryancillaryservices/LineItemSpecification.java
@@ -9,8 +9,7 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class
-LineItemSpecification implements Serializable {
+public class LineItemSpecification implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private String broadbandService;
